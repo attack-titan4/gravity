c0fce49a4add1333e845e06285281012a9e0ce22
Sulabh Jha <66020896+sulabh007@users.noreply.github.com>
2023-10-09 22:17:12 +0530

    Add files via upload
    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,15 +5,11 @@ import './Footer.css'; // Import the CSS file for styles
 
 const Footer = () => {
   return (
-    <>
-      
+    <div className="footer">
+      <div className="test-style">TAG US TO GET A CHANCE TO GET FEATURED</div>
 
       <div className="company-name">
         <img src="/Resources/dressmart.png" alt="Dressmart Logo" />
-      </div>
-
-      <div className="test-style">
-        TAG US TO GET A CHANCE TO GET FEATURED
       </div>
 
       <div className="test-style1">
@@ -22,44 +18,75 @@ const Footer = () => {
       </div>
 
       <div className="company-name1">
-        Due to the impact of Covid-19, we are currently experiencing minor delays with our shipping carrier 
-        <div className="shipping">Free shipping over $999<br/>Free 5-day return shipping</div>
+        Due to the impact of Covid-19, we are currently experiencing minor
+        delays with our shipping carrier
+        <div className="shipping">
+          Free shipping over $999
+          <br />
+          Free 5-day return shipping
+        </div>
       </div>
-
-      
 
       <div className="footer-style2">
         <div className="social-media-icons">
-          <a href="https://www.facebook.com"><FaFacebook className="icon-style" /></a>
-          <a href="https://www.twitter.com"><FaTwitter className="icon-style" /></a>
-          <a href="https://www.instagram.com"><FaInstagram className="icon-style" /></a>
-          <a href="https://www.pinterest.com"><FaPinterest className="icon-style" /></a>
+          <a href="https://www.facebook.com">
+            <FaFacebook className="icon-style" />
+          </a>
+          <a href="https://www.twitter.com">
+            <FaTwitter className="icon-style" />
+          </a>
+          <a href="https://www.instagram.com">
+            <FaInstagram className="icon-style" />
+          </a>
+          <a href="https://www.pinterest.com">
+            <FaPinterest className="icon-style" />
+          </a>
         </div>
       </div>
 
       <div className="footer-style">
-        <NavLink to="/" className="nav-link">Sign in</NavLink>
-        <NavLink to="/" className="nav-link">Shipping</NavLink>
-        <NavLink to="/" className="nav-link">Returns</NavLink>
-        <NavLink to="/" className="nav-link">Help</NavLink>
-        <NavLink to="/" className="nav-link">Contact Us</NavLink>
-        <NavLink to="/" className="nav-link">Size Guide</NavLink>
+        <NavLink to="/" className="nav-link">
+          Sign in
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Shipping
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Returns
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Help
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Contact Us
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Size Guide
+        </NavLink>
       </div>
 
       <div className="footer-style1">
-        <NavLink to="/" className="nav-link">About Us</NavLink>
-        <NavLink to="/" className="nav-link">Careers</NavLink>
-        <NavLink to="/" className="nav-link">Dressmart Cares</NavLink>
+        <NavLink to="/" className="nav-link">
+          About Us
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Careers
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Dressmart Cares
+        </NavLink>
       </div>
 
       <div className="footer-links">
-        <Link className="nav-link">Sitemap</Link> | <Link className="nav-link">Terms of Service</Link> | <Link className="nav-link">Privacy Policy</Link>
+        <Link className="nav-link">Sitemap</Link> |{" "}
+        <Link className="nav-link">Terms of Service</Link> |{" "}
+        <Link className="nav-link">Privacy Policy</Link>
       </div>
 
       <div className="copyright">
         © 2022 dressmart.com LLC. ALL RIGHTS RESERVED.
       </div>
-    </>
+    </div>
   );
 };
 
