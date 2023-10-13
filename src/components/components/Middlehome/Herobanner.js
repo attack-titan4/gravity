7fd615be3997a67303bcd2ee6a5b959a9b137e04
Sulabh Jha <66020896+sulabh007@users.noreply.github.com>
2023-10-13 21:06:44 +0530

    Add files via upload

---
+++ src/components/components/Middlehome/Herobanner.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Herobanner.css"
+
+const Herobanner = () => {
+  return (
+    <div className="container">
+      <div className="carausel">
+        <div className="image-container">
+          <img
+            className="banner"
+            src="\Resources\productimages\p4.png"
+            alt="ops"
+          />
+        </div>
+        <div className="text-container">
+          <h2 className="drop">First Drop Coming Soon</h2>
+          <div className="coming">
+            Join our exclusive waitlist for the coming drop
+          </div>
+          <button className="join">Join Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Herobanner
